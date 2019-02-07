487f766affe19ec80af1ed871e7309c150200bfd
shiblee86 <syed.r.abdullah@gmail.com>
2019-02-07 16:27:18 -0500

    Assertion

--- src/com/systemsetup/RunTestCases.java
+++ src/com/systemsetup/RunTestCases.java
@@ -2,6 +2,8 @@ package com.systemsetup;
 
 import java.io.IOException;
 
+import org.testng.Assert;
+
 public class RunTestCases {
 
 	static UserGetsElementFromAllList obj;
@@ -21,11 +23,12 @@ public class RunTestCases {
 		System.out.println("********************************");
 		System.out.println("List from Amazon");
 		System.out.println("********************************");
-		obj.getDropDownList();
+		System.out.println(obj.getDropDownList());
+		;
 		System.out.println("********************************");
 		System.out.println("List from File");
 		System.out.println("********************************");
-		obj1.dropdownListFromFile();
+		System.out.println(obj1.dropdownListFromFile());
 
 		if (obj.getDropDownList().equals(obj1.dropdownListFromFile())) {
 			System.out.println("********************************");
@@ -33,6 +36,12 @@ public class RunTestCases {
 		} else {
 			System.out.println("********************************");
 			System.out.println("Drop-downs DO NOT match");
+		}
+
+		try {
+			Assert.assertEquals(obj.getDropDownList(), obj1.dropdownListFromFile());
+		} catch (Exception e) {
+			System.out.println("Values between dropdown and Excel file do not match");
 		}
 	}
 }
